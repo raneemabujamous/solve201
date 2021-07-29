410833aa74039449fcc38e90582c52a9825c5042
raneemabujamous <raneemabujamous1998@gmail.com>
2021-07-29 19:06:11 +0300

    added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b ) { //eslint-disable-line
   let total = a + b;
   return [total,  `The sum of ${a} and ${b} is ${total}.`];
 }
@@ -23,7 +23,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b ) { //eslint-disable-line
     let product = a * b;
     return [ product, `The product of ${a} and ${b} is ${product}.`];
 }
@@ -43,11 +43,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    let x = sum(a, b);
+    let y = x[0];
+    let z = sum(y, c);
+    let sumOfelements = z[0];
+  
+    let s = multiply(a, b);
+    let m = s[0];
+    let n = multiply(m, c);
+    let productsOfelements = n[0];
+  
+    return [sumOfelements , productsOfelements , `${a} and ${b} and ${c} sum to ${sumOfelements}.`,`The product of ${a} and ${b} and ${c} is ${productsOfelements}.`];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
